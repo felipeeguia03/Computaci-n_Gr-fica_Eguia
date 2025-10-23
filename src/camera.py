3cbbdbb9ed4acd99c05178a76f2025706939d1f8
Felipe Eguia <felipeeguia03@gmail.com>
2025-10-23 12:40:39 -0300

    no anda cpu

--- src/camera.py
+++ src/camera.py
@@ -1,5 +1,6 @@
-import glm
 from ray import Ray
+import glm
+
 
 # ultima si dios quiere
 
@@ -36,13 +37,13 @@ class Camera:
     def raycast(self, u, v):
         fov_adjustment = glm.tan(glm.radians(self.fov) / 2)
         ndc_x = (2 * u - 1 ) * self.aspect * fov_adjustment
-        ndc_y = (1 - 2 * v) * fov_adjustment
+        ndc_y = (2 * v - 1) * fov_adjustment
 
         ray_dir_camera = glm.vec3(ndc_x, ndc_y, -1.0)
         ray_dir_camera = glm.normalize(ray_dir_camera)
 
         view = self.get_view_matrix()
         inv_view = glm.inverse(view)
-        ray_dir_world = glm.vec3(inv_view * glm.vec4(ray_dir_camera,0.0))
+        ray_dir_world = glm.normalize(glm.vec3(inv_view * glm.vec4(ray_dir_camera,0.0)))
 
         return Ray(self.position,ray_dir_world)
